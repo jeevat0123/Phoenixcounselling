20b0a4d6184a20cf3ea846293512149e19367d8c
jeevat0123 <jeevat0123@outlook.com>
2024-11-15 22:01:06 +0530

    wip: img alter

--- Phoenix/src/app/services/calendly/calendly.service.ts
+++ Phoenix/src/app/services/calendly/calendly.service.ts
@@ -4,7 +4,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root',
 })
 export class CalendlyService {
-  openCalendlyPopup(url: string = 'https://calendly.com/uma_phoenix_counselling'): void {
+  openCalendlyPopup(url: string = 'https://calendly.com/phoenix_counselling/phoenix-counselling'): void {
     const calendly = (window as any).Calendly as { initPopupWidget: (options: { url: string }) => void };
     if (calendly) {
       calendly.initPopupWidget({ url });
